34b06c1a948f2500183b836966387a1ab1701ee3
Kyle McKee <mckee.kyle@gmail.com>
2018-12-16 16:28:20 -0500

    Works

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import Fireworks from 'fireworks-canvas';
+import './main.css';
+
 export default () => {
-  alert('it works!');
+  const container = document.getElementById('container');
+  const fireworks = new Fireworks(container);
+  fireworks.start();
 };
